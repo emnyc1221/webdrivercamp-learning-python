5fcbd870bac5ff79f3a41e0f7951912d07e97f1c
emnyc1221 <144756095+emnyc1221@users.noreply.github.com>
2023-10-16 20:21:48 -0400

    Update 6_what_needed.py

--- python_beginning/6_what_needed.py
+++ python_beginning/6_what_needed.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 my_string = "There should be one-- and preferably only one --obvious way to do it. Although that way may not be obvious at first unless you're Dutch."
 
 modified_string = my_string.split ("and")[1].split(".")[0].strip()
