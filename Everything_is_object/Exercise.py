6ce80a5a8712e43b531d7b2b755b7d5d1c2ed917
emnyc1221 <emnyc1221@gmail.com>
2023-11-20 21:13:31 -0500

    Python - Selenium - XPath

--- Everything_is_object/Exercise.py
+++ Everything_is_object/Exercise.py
@@ -1,10 +1,10 @@
 ///#0. What function would you use to print the type of an object?///
-x = [1, 2, 3]
-print(type(x))
+#x = [1, 2, 3]
+#print(type(x))
 
 ///#1. How do you get the variable identifier (which is the memory address in the CPython implementation)?
-x = 'hello'
-print(id(x))///
+#x = 'hello'
+#print(id(x))///
 
 ///#2. In the following code, do a and b point to the same object?
 >>> a = 89
